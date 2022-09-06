e1b7e303409c68c73b196dfbe683e3e834acb1ea
Alex0622 <70553543+Alex22sv@users.noreply.github.com>
2022-09-06 15:23:17 -0600

    Update website
    + Update description about me
    - Hide countdown until my birthday.

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,44 +114,44 @@
 
 // Birthday countdown
 // Credits: Code With Random (https://www.codewithrandom.com/)
-(function () {  
-	const second = 1000,  
-	   minute = second * 60,  
-	   hour = minute * 60,  
-	   day = hour * 24;  
-	//Avoid having to update year when the countdown is over
-	let today = new Date(),  
-	  dd = String(today.getDate()).padStart(2, "0"),  
-	  mm = String(today.getMonth() + 1).padStart(2, "0"),  
-	  yyyy = today.getFullYear(),  
-	  nextYear = yyyy + 1,  
-	  dayMonth = "06/22/",  
-	  birthday = dayMonth + yyyy;  
-	today = mm + "/" + dd + "/" + yyyy;  
-	if (today > birthday) {  
-	 birthday = dayMonth + nextYear;  
-	}  
+// (function () {  
+// 	const second = 1000,  
+// 	   minute = second * 60,  
+// 	   hour = minute * 60,  
+// 	   day = hour * 24;  
+// 	//Avoid having to update year when the countdown is over
+// 	let today = new Date(),  
+// 	  dd = String(today.getDate()).padStart(2, "0"),  
+// 	  mm = String(today.getMonth() + 1).padStart(2, "0"),  
+// 	  yyyy = today.getFullYear(),  
+// 	  nextYear = yyyy + 1,  
+// 	  dayMonth = "06/22/",  
+// 	  birthday = dayMonth + yyyy;  
+// 	today = mm + "/" + dd + "/" + yyyy;  
+// 	if (today > birthday) {  
+// 	 birthday = dayMonth + nextYear;  
+// 	}  
 	
-	//Countdown 
-	const countDown = new Date(birthday).getTime(),  
-	  	x = setInterval(function() {    
-	    const now = new Date().getTime(),  
-		distance = countDown - now;  
-	    document.getElementById("days").innerText = Math.floor(distance / (day)),  
-		document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),  
-		document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),  
-		document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);  
-	   //It's my birthday 
-	   if (distance < 0) {  
-		document.getElementById("countdownTitle").style.display = "none"; 
-		document.getElementById("countdownOverTitle").style.display = "block"; 
-		document.getElementById("countdown").style.display = "none";  
-		document.getElementById("emojiContent").classList.remove("hidden");  
-		clearInterval(x);  
-	   }  
-	   //seconds  
-	  }, 0)  
-	}());  
+// 	//Countdown 
+// 	const countDown = new Date(birthday).getTime(),  
+// 	  	x = setInterval(function() {    
+// 	    const now = new Date().getTime(),  
+// 		distance = countDown - now;  
+// 	    document.getElementById("days").innerText = Math.floor(distance / (day)),  
+// 		document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),  
+// 		document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),  
+// 		document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);  
+// 	   //It's my birthday 
+// 	   if (distance < 0) {  
+// 		document.getElementById("countdownTitle").style.display = "none"; 
+// 		document.getElementById("countdownOverTitle").style.display = "block"; 
+// 		document.getElementById("countdown").style.display = "none";  
+// 		document.getElementById("emojiContent").classList.remove("hidden");  
+// 		clearInterval(x);  
+// 	   }  
+// 	   //seconds  
+// 	  }, 0)  
+// 	}());  
 
 // Create a function to change
 // the hash value of the page
@@ -169,7 +169,7 @@ var language = {
 		navprojects: "Projects",
 		navcontact: "Contact",
 		titleaboutme: "About me",
-		descaboutme: "Hi! I'm Alexander from El Salvador, and I like to create programs, mostly websites but I've also developed Discord bots in the past. I'm interested on working on new and innovative projects. I can work alone and with teams. My native language is Spanish but I can fluently speak and understand English. In my free time I like to play videogames (mostly CoD and Rocket League) but also listen to Lofi music.",
+		descaboutme: "Hi! I'm Alexander from El Salvador, and I like to create programs, mostly websites but I've also developed Discord bots in the past. I'm currently learning how to create and develop Minecraft plugins for Minecraft servers. I'm interested on working on new and innovative projects. I can work alone and with teams. My native language is Spanish but I can fluently speak and understand English. In my free time I like to play videogames (mostly CoD and Rocket League) but also listen to Lofi music.",
 		countdowntitle: "Countdown to my birthday",
 		countdownovertitle: "It's my birthday!",
 		daystext: "days",
@@ -210,7 +210,7 @@ var language = {
 		navprojects: "Proyectos",
 		navcontact: "Contacto",
 		titleaboutme: "Acerca de mí",
-		descaboutme: "Hola! mi nombre es Alexander y soy de El Salvador. Me gusta crear programas, más que nada sitios web, aunque también he desarrollado bots de Discord en el pasado. Estoy interesado en trabajar en proyectos nuevos e inovadores. Suelo trabajar solo pero puedo organizarme con equipos. Puedo hablar el inglés y el español. En mi tiempo libre suelo jugar videojuegos (principalmente Call of Duty y Rocket League) pero también escuchar música Lofi.",
+		descaboutme: "Hola! mi nombre es Alexander y soy de El Salvador. Me gusta crear programas, más que nada sitios web, aunque también he desarrollado bots de Discord en el pasado. Actualmente me encuentro estudiando el desarrollo de plugins para servidores de Minecraft. Estoy interesado en trabajar en proyectos nuevos e inovadores. Suelo trabajar solo pero puedo organizarme con equipos. Puedo hablar el inglés y el español. En mi tiempo libre suelo jugar videojuegos (principalmente Call of Duty y Rocket League) pero también escuchar música Lofi.",
 		countdowntitle: "Cuenta regresiva a mi cumpleaños",
 		countdownovertitle: "¡Es mi cumpleaños!",
 		daystext: "días",
@@ -257,12 +257,12 @@ if (window.location.hash) {
 		document.getElementById("navContact").textContent = language.eng.navcontact;
 		document.getElementById("titleAboutMe").textContent = language.eng.titleaboutme;
 		document.getElementById("descAboutMe").textContent = language.eng.descaboutme;
-		document.getElementById("countdownTitle").textContent = language.eng.countdowntitle;
-		document.getElementById("countdownOverTitle").textContent = language.eng.countdownovertitle;
-		document.getElementById("daysText").textContent = language.eng.daystext;
-		document.getElementById("hoursText").textContent = language.eng.hourstext;
-		document.getElementById("minutesText").textContent = language.eng.minutestext;
-		document.getElementById("secondsText").textContent = language.eng.secondstext;
+		// document.getElementById("countdownTitle").textContent = language.eng.countdowntitle;
+		// document.getElementById("countdownOverTitle").textContent = language.eng.countdownovertitle;
+		// document.getElementById("daysText").textContent = language.eng.daystext;
+		// document.getElementById("hoursText").textContent = language.eng.hourstext;
+		// document.getElementById("minutesText").textContent = language.eng.minutestext;
+		// document.getElementById("secondsText").textContent = language.eng.secondstext;
 		document.getElementById("thingsILikeToDoTitle").textContent = language.eng.thingsiliketodotitle;
 		document.getElementById("writeCode").textContent = language.eng.writecode;
 		document.getElementById("playVideogames").textContent = language.eng.playvideogames;
